129b87feab7f4b9f13d214f5fc430badf8402c52
arun050775-code <arun050775@gmail.com>
2026-06-06 16:29:29 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import AKSWebsite from './App.jsx'
 import Portal from './portal/Portal.jsx'
 
 const isPortal = window.location.pathname.startsWith('/portal')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {isPortal ? <Portal /> : <App />}
+    {isPortal ? <Portal /> : <AKSWebsite />}
   </React.StrictMode>
 )
